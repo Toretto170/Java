9fd2605b6844c76f808d5d47e18a43baf707ecce
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-06-14 13:06:32 +0200

    ascd

---
+++ Prj22_Bollettini_Postali/src/test/java/com/spadea/Prj22BollettiniPostaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spadea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj22BollettiniPostaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
